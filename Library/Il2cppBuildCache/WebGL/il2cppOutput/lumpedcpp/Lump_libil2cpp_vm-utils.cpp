e0ca10f055d3e86f34986546cb4e56dddfac8e01
Zhao4 <102609240+WanzhuZhao@users.noreply.github.com>
2022-04-19 09:26:04 -0600

    Change of the main menu. Updated the content of the Help information, and the location of the Help button.

--- Library/Il2cppBuildCache/WebGL/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\BlobReader.cpp"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\Debugger.cpp"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "E:\Unity\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
